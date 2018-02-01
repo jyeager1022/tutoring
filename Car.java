09a0c7c514e217da5e92a29bbf0c70f481a62aa7
jyeager1022 <36018339+jyeager1022@users.noreply.github.com>
2018-01-31 17:28:55 -0700

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,33 @@
+
+public class Car {
+
+	private String make;
+	private String model;
+	
+	public Car(String make, String model){
+			this.make = make;
+			this.model = model;
+		}
+		
+		public String getMake(){
+			return this.make;
+		}
+		
+		public String getModel(){
+			return this.model;
+			
+		}
+			
+		public void setMake(String make){
+			 this.make = make;
+						
+		}
+
+		public void setModel(String model) {
+			this.model = model;
+		}
+
+	
+
+
+}
