e45b56f8394cfabccde240fb4bb68acd6f2c8f8f
jyeager1022 <36018339+jyeager1022@users.noreply.github.com>
2018-01-31 17:28:15 -0700

    Create Testdrive.java

---
+++ Testdrive.java
@@ -0,0 +1,28 @@
+
+public class Testdrive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Car charger = new Car("dodge", "charger");
+	Car wrangler = new Car("jeep","wrangler");
+	Car sl1 = new Car("saturn","sl1");
+	Car f150 = new Car("ford","f150");
+	Car volt = new Car("chevy","volt");
+
+	System.out.println(charger.getMake());	
+	
+	}
+
+	
+	
+	
+		
+		
+		
+		
+	}
+	
+	
+}
+
